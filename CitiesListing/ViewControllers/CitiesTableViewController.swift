b7074389672aae7f009904ae353febe7acd7b856
Rodrigo Bueno Tomiosso <mourodrigo_tres@rodrigos-mac-mini.local>
2018-03-12 13:42:23 -0300

    Cities file loading and JSON parsing to cities array

--- CitiesListing/ViewControllers/CitiesTableViewController.swift
+++ CitiesListing/ViewControllers/CitiesTableViewController.swift
@@ -10,26 +10,52 @@ import UIKit
 
 class CitiesTableViewController: UITableViewController {
 
+    var allCities = [City]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadAllCities()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        allCities.removeAll()
     }
 
     // MARK: - Table view data source
+    func loadAllCities() {
+        // this structure provides the possibility to continuously add cities from more json files, for instance from a paginated API
+        allCities.removeAll()
+        allCities.append(contentsOf: getCities(from: CitiesData.contentString))
+    }
+    
+    func getCities(from jsonString:String) -> Array<City> {
+        if let data: Data = jsonString.data(using: String.Encoding.utf8) as Data? {
+            let jsonObject = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
+            return self.parseJson(anyObj: jsonObject)
+        }
+        return Array<City>()
+    }
+    
+    func parseJson(anyObj:Any) -> Array<City> {
+        var list:Array<City> = []
+        if  anyObj is Array<AnyObject> {
+            for json in anyObj as! Array<AnyObject>{
+                guard let coord = json["coord"] as? Dictionary<AnyHashable,Double> else { break }
+                let newCity = City(id: (json["_id"]  as AnyObject? as? Int) ?? 0,
+                                     name: (json["name"] as AnyObject? as? String) ?? "",
+                                     country: (json["country"] as AnyObject? as? String) ?? "",
+                                     latitude: coord["lat"] ?? 0.00,
+                                     longitude: coord["lon"] ?? 0.00
+                )
+                list.append(newCity)
+            }
+        }
+        return list
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
